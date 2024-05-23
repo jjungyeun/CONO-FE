3ad4d205a725d86d677af8cc71cdc8cbd26fd851
jungyeunwon <wjyddd@naver.com>
2024-05-23 18:35:49 +0900

    feat #1 - 홈화면 구현
    - 홈화면에서는 헤더 안보이게 설정
    - 로고 및 버튼 2개 삽입
    - 버튼 클릭 시 /search로 이동하도록 라우터 링크 설정

---
+++ src/components/HomePage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" />
+    <table>
+      <tr>
+        <td>
+          <router-link to="/search">
+            <div style="background-color: #e4f2ff">노래방<br />찾기</div>
+          </router-link>
+        </td>
+        <td>
+          <router-link to="/search">
+            <div style="background-color: #feecff">내 근처<br />노래방</div>
+          </router-link>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  background-color: #ffffe4;
+  text-align: center;
+  min-height: 90vh;
+  align-content: center;
+}
+
+img {
+  width: 35%;
+}
+
+table {
+  border-spacing: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+  height: 200px;
+}
+
+td {
+  width: 50%;
+
+  div {
+    height: 100%;
+    align-content: center;
+    border-radius: 20px;
+
+    font-size: x-large;
+    color: black;
+  }
+}
+</style>
