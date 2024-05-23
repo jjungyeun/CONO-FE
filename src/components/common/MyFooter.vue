f4d8402a23cdd235018e6950525c27b18727c6c8
jungyeunwon <wjyddd@naver.com>
2024-05-23 18:34:59 +0900

    feat #1 - 헤더 및 푸터 구현
    - 헤더&푸터 구현 및 app.vue에 적용

---
+++ src/components/common/MyFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="nav">
+    <p>동내코노 created by <a href="https://github.com/jjungyeun" target="_blank">jjungyeun</a></p>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: gray;
+  font-style: italic;
+}
+</style>
---
+++ src/components/common/MyHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="header-wrapper">
+      <router-link to="/" v-show="$route.path != '/'"
+        ><img class="header-logo" src="../../assets/logo.png"
+      /></router-link>
+    </div>
+  </header>
+</template>
+
+<style>
+header {
+  background-color: #ffffe4;
+}
+
+.header-logo {
+  width: 30%;
+}
+
+.header-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 50px;
+  padding: 0 16px;
+}
+</style>
