e15eff17f589ea2b71e51a89e8c967d04bae52ef
jungyeunwon <wjyddd@naver.com>
2024-06-24 21:52:25 +0900

    feat #7 - axios 관련 설정 추가
    - app에 axios 등록 및 전역 변수로 추가
    - 백엔드 API 호출용 프록시 주소 설정

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      // 이 설정을 사용할 수 있도록 api prefix를 추가한다.
+      // 사용 코드에서 이 prefix를 붙여서 사용하면, rewrite 코드가 떼고 서버로 보낸다.
+      '/be-api': {
+        target: 'http://localhost:8080',
+        rewrite: (path) => path.replace('/be-api', '/api')
+      }
     }
   }
 })
